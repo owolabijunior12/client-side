c2765e517c422b0f1a44dfa4a0938b3faff5205b
Owolabi Destiny <107050551+owolabijunior12@users.noreply.github.com>
2023-02-11 14:57:10 +0100

    add vadilation to the from

--- src/component/login.js
+++ src/component/login.js
@@ -1,10 +1,16 @@
 
 import React from "react";
+import { Form, Button } from 'semantic-ui-react';
+import { useForm } from "react-hook-form";
 // import IBOY from "./IBOY.jpg"
 import "../style/Login.css";
 import { accessUrl } from "./sportify.js";
 
 function Login() {
+      const { register, handleSubmit, formState: { errors } } = useForm();
+      const onSubmit = (data) => {
+          console.log(data);
+      }
   return (
     <div className="login">
       <img
@@ -12,28 +18,52 @@ function Login() {
         alt=""
       /> 
       <div>
-          <form>
-                <div className="from-control">
-                      <label >UserName</label>
-                      <input type='text' placeholder="UserName"/>
-                      <small className="err">username cannot be empty</small>
-                </div>
-                <div className="from-control">
-                      <label >Full Name</label>
-                      <input type='text' placeholder="Full Name"/>
-                      <small className="err">full name cannot be empty</small>
-                </div>
-                <div className="from-control">
-                      <label >Password</label>
-                      <input type='password' placeholder="Password"/>
-                      <small className="err">password can not be empty</small>
-                </div>
-                <div className="from-control">
-                      <label >Comfirm Password</label>
-                      <input type='password' placeholder="CmPassword" />
-                      <small className="err">password is not corresponing to the password created</small>
-                </div>
-          </form>
+             <Form onSubmit={handleSubmit(onSubmit)}>
+                <Form.Field>
+                    <label>First Name</label>
+                    <input
+                        placeholder='First Name'
+                        type="text"
+                        {...register("firstName", { required: true, maxLength: 10 })}
+                    />
+                </Form.Field>
+                {errors.firstName && <p>Please check the First Name</p>}
+                <Form.Field>
+                    <label>Last Name</label>
+                    <input
+                        placeholder='Last Name'
+                        type="text"
+                        {...register("lastName", { required: true, maxLength: 10 })}
+                    />
+                </Form.Field>
+                {errors.lastName && <p>Please check the Last Name</p>}
+                <Form.Field>
+                    <label>Email</label>
+                    <input
+                        placeholder='Email'
+                        type="email"
+                        {...register("email",
+                            {
+                                required: true,
+                                pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                            })}
+                    />
+                </Form.Field>
+                {errors.email && <p>Please check the Email</p>}
+                <Form.Field>
+                    <label>Password</label>
+                    <input
+                        placeholder='Password'
+                        type="password"
+                        {...register("password", {
+                            required: true,
+                            pattern: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,15}$/
+                        })}
+                    />
+                </Form.Field>
+                {errors.password && <p>Please check the Password</p>}
+                <Button type='submit'>Submit</Button>
+            </Form>
       </div>
        <a href={accessUrl}>LOGIN </a>
     </div>
